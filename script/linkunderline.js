340cf7a6e788af7adc4a9b5b7951898e4ef320b5
Mike <romanosir@yandex.ru>
2024-12-03 17:12:10 +0300

    change color link by clicling

--- script/linkunderline.js
+++ script/linkunderline.js
@@ -1,5 +1,13 @@
-$('.nav-link').click(function() {
-    $('.nav-link').removeClass('underlined-link');
-    $(this).addClass('underlined-link');
-    return false;
- });
+document.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('.nav-link'); // Все ссылки навигации
+    const currentUrl = window.location.pathname; // Получаем текущий путь (например, /about.html)
+
+    links.forEach(link => {
+        // Если href ссылки совпадает с текущим путем
+        if (link.getAttribute('href') === currentUrl) {
+            link.classList.add('underlined-link'); // Добавляем класс для подсветки
+            link.classList.add('.nav-link:after');
+        }
+
+    });
+});
