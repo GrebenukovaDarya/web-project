6fa418288c5d54ff17409de8d58541288361afac
Mike <romanosir@yandex.ru>
2024-12-04 12:25:46 +0300

    update underline links JS

--- script/linkunderline.js
+++ script/linkunderline.js
@@ -11,3 +11,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
     });
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('.down-links'); // Все ссылки навигации
+    const currentUrl = window.location.pathname; // Получаем текущий путь (например, /about.html)
+
+    links.forEach(link => {
+        // Если href ссылки совпадает с текущим путем
+        if (link.getAttribute('href') === currentUrl) {
+            link.classList.add('changecolor-link'); // Добавляем класс для подсветки
+            
+        }
+
+    });
+});
